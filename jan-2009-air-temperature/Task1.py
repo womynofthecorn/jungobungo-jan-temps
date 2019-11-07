6bfed077be0d7d15d7a4e51fe89d39b0ff033df6
elisevu <44879175+elisevu@users.noreply.github.com>
2019-11-07 14:23:26 -0500

    fixedish
    errors with _geoslib dll

--- jan-2009-air-temperature/Task1.py
+++ jan-2009-air-temperature/Task1.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from netCDF4 import Dataset
-import mpl_toolkits.basemap as bm
-sns.set()
-nc = Dataset('ta_2m_1hr_20090101_d02.nc', 'r')
+from netCDF4 import Dataset as Ncdf
+from mpl_toolkits.basemap import Basemap
+nc = Ncdf('ta_2m_1hr_20090101_d02.nc', 'r')
 print(nc)
 for i in nc.variables:
     print(i, nc.variables[i].units, nc.variables[i].shape)
-
 lon = nc.variables['lon'][:]
 lat = nc.variables['lat'][:]
 time = nc.variables['time'][:]
-lons,lats = np.meshgrid(lon-180,lat)
-map = bm.Basemap(projection='merc', llcrnrlon=-93.,llcrnrlat=35.,urcrnrlon=-73.,urcrnrlat=45.,resolution='i')
-x,y = map(lons,lats)
-temp = map.contourf(x,y,t2[4, :, :])
-plt.title('2m Temperature')
-cb.set_label('Temperature (K)')
-plt.show()
+t2 = nc.variables['2m'][:]
+map = Basemap(projection='merc',llcrnrlon=-93.,llcrnrlat=35.,urcrnrlon=-73.,urcrnrlat=45.,resolution='i')
+map.drawcoastlines()
+map.drawstates()
+map.drawcountries()
+map.drawlsmask(land_color='Linen', ocean_color='#CCFFFF')
+lon, lat = np.meshgrid(lon-180, lat)
+x, y = map(lon, lat)
+temp = map.contourf(x, y, t2[4,:, :])
 cb = map.colorbar(temp,"bottom", size="5%", pad="2%")
+plt.title('2m Temperature')
+cb.set_label('Temperature (C)')
+plt.show()
 plt.savefig("E:/elise/Documents/JungoBunbo/jan-2009-air-temperature/figure.png", 100)
-zip()
+
