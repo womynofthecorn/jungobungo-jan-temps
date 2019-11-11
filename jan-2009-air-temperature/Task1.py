53dd1204a9e18f4af6adac63034cd0bcd9fbc301
elisevu <44879175+elisevu@users.noreply.github.com>
2019-11-11 16:28:24 -0500

    fixed

--- jan-2009-air-temperature/Task1.py
+++ jan-2009-air-temperature/Task1.py
@@ -2,25 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset as Ncdf
 from mpl_toolkits.basemap import Basemap
+import matplotlib
+matplotlib.use("Agg")
 nc = Ncdf('ta_2m_1hr_20090101_d02.nc', 'r')
 print(nc)
 for i in nc.variables:
     print(i, nc.variables[i].units, nc.variables[i].shape)
-lon = nc.variables['lon'][:]
-lat = nc.variables['lat'][:]
+lons = nc.variables['lon'][:]
+lats = nc.variables['lat'][:]
 time = nc.variables['time'][:]
-t2 = nc.variables['2m'][:]
-map = Basemap(projection='merc',llcrnrlon=-93.,llcrnrlat=35.,urcrnrlon=-73.,urcrnrlat=45.,resolution='i')
-map.drawcoastlines()
-map.drawstates()
-map.drawcountries()
-map.drawlsmask(land_color='Linen', ocean_color='#CCFFFF')
-lon, lat = np.meshgrid(lon-180, lat)
-x, y = map(lon, lat)
-temp = map.contourf(x, y, t2[4,:, :])
-cb = map.colorbar(temp,"bottom", size="5%", pad="2%")
-plt.title('2m Temperature')
-cb.set_label('Temperature (C)')
-plt.show()
-plt.savefig("E:/elise/Documents/JungoBunbo/jan-2009-air-temperature/figure.png", 100)
-
+t2 = nc.variables['ta_2m'][:]
+map = Basemap(projection='merc', llcrnrlon=lons[0], llcrnrlat=lats[0], urcrnrlon=lons[599], urcrnrlat=lats[698],
+             resolution='i')
+#map.drawcoastlines()
+# map.drawstates()
+#map.drawcountries()
+#map.drawlsmask(land_color='Linen', ocean_color='#CCFFFF')
+lon2, lat2 = np.meshgrid(lons, lats)
+x, y = map(lon2, lat2)
+my_cmap = plt.get_cmap('RdBu_r')
+my_cmap.set_under('white')
+map.pcolormesh(x, y, t2[0, :, :], cmap='RdBu_r')
+plt.colorbar(label="Temperature (K)")
+for b in range(0, 744):
+    map.pcolormesh(x, y, t2[b, :, :], cmap='RdBu_r')
+    plt.title('2m Temperature')
+    plt.savefig("E:/elise/Documents/JungoBunbo/jan-2009-air-temperature/ta2m_%s.png" % b)
